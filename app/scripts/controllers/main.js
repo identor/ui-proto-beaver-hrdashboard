dda007e836d0e282656e7bf950cb8308ff02bd88
Irvin Denzel Torcuato <irvinisready@gmail.com>
2014-10-18 18:44:47 +0800

    initial yeoman scafold

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name leadScoreclientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the leadScoreclientApp
+ */
+angular.module('leadScoreclientApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
