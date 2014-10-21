c64e56ec31d38ab2fe339d445537413a2c7f9441
Irvin Denzel Torcuato <irvinisready@gmail.com>
2014-10-21 18:08:03 +0800

    can now read files

--- app/scripts/controllers/upload.js
+++ app/scripts/controllers/upload.js
@@ -9,9 +9,20 @@
  */
 angular.module('leadScoreClientApp')
   .controller('UploadCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    var $leadScoreFileInput = $('#leadscore-file-input');
+    var $leadScoreUploadButton = $('#leadscore-upload-button');
+    $scope.file = '123123';
+    $scope.updateLeadScoreFile = function() {
+      $scope.file = $leadScoreFileInput.get(0).files[0];
+      $scope.fileName = $scope.file.name;
+      console.log('file selected');
+    };
+
+    $leadScoreFileInput.on('change', function() {
+      $scope.updateLeadScoreFile();
+      $scope.$apply();
+    });
+    $leadScoreUploadButton.on('click', function() {
+      console.log('update called ' + $scope.file.name);
+    });
   });
