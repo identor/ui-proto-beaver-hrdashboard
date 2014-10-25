1ee687b0581212ca7e31aaa5c22b74360596d51d
Irvin Denzel Torcuato <irvinisready@gmail.com>
2014-10-25 18:23:05 +0800

    personnel template files generated

---
+++ app/scripts/controllers/personnel.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name leadScoreClientApp.controller:PersonnelCtrl
+ * @description
+ * # PersonnelCtrl
+ * Controller of the leadScoreClientApp
+ */
+angular.module('leadScoreClientApp')
+  .controller('PersonnelCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
